7e67dfa66240e69d190ebfd4dc35d76a67a60a11
German Breus <german.breus@gmail.com>
2015-11-25 22:03:23 +0200

    Implemented classes and libraries correctly.

--- src/CMM/Main.java
+++ src/CMM/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage loginStage) throws Exception {
         new LoginCMM();
     }
 
